87433d162238647a71e4672c7f7161452ab67b62
David Parsons <david.parsons@rutgers.edu>
2018-01-12 23:23:25 -0500

    Start Backend

---
+++ src/backend/Problem.js
@@ -0,0 +1,28 @@
+// Class for allowing the Frontend and User to interact with 
+// the automatically generated problems.
+class Problem{
+
+  constructor(){
+    this.isSolved = false;
+
+  }
+
+  // Gets the problem to be displayed.
+  // Either the HTML or an object representing that can easily be translated into HTML
+  getProblem(){
+
+  }
+
+  // Tests answer against the problem solution
+  // Setting the instance to solved if the answer is correct
+  testAnswer(answer){
+
+  }
+
+  // Gets the correct solution to the problem
+  getSolution(){
+
+  }
+
+
+}
